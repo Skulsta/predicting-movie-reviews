38414e6e9bb28ec4f48aec8bae15398c9f19b228
Skulsta <sti.sku@gmail.com>
2018-02-28 09:42:50 +0100

    added a test to show whether something exists and is a file.

--- Training.py
+++ Training.py
@@ -1,9 +1,16 @@
 from collections import Counter
 
-# os path
+import os.path
 
+# Gets the first file from pos training folder and
+# prints the data.
 text_file = open("aclImdb/train/pos/0_9.txt", 'r')
 lines = text_file.readlines()
 print(lines)
 print(len(lines))
 text_file.close()
+
+# true if this exists and is a file, false if not.
+print(os.path.isfile("aclImdb/train/pos/0_9.txt"))
+
+
