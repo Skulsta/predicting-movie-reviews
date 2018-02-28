f253bed0596b12ab410880838d77f311116ac442
Skulsta <sti.sku@gmail.com>
2018-02-28 09:51:41 +0100

    Added helper methods to give some kind of overview of what we have to work with.

--- Training.py
+++ Training.py
@@ -1,3 +1,5 @@
+# Importing this class because it will likely be usefull
+# for counting the number of times an item occur in a list.
 from collections import Counter
 
 import os.path
@@ -12,5 +14,17 @@ text_file.close()
 
 # true if this exists and is a file, false if not.
 print(os.path.isfile("aclImdb/train/pos/0_9.txt"))
+
+
+# List every file in the pos training folder
+positive_reviews = os.listdir("aclImdb/train/pos")
+print(positive_reviews)
+
+
+# Counting every file in the folder
+counter = 1
+for file in positive_reviews:
+   print(counter)
+   counter += 1
 
 
