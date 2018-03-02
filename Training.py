542c1ee4322e70bc8c62d4bbc0e461035acecc44
Skulsta <sti.sku@gmail.com>
2018-03-02 11:46:58 +0100

    Counts something that might be the number of positive words in a review (unlikely tho)

--- Training.py
+++ Training.py
@@ -3,6 +3,7 @@
 from collections import Counter
 
 import os.path
+import re
 
 # Gets the first file from pos training folder and
 # prints the data. Also, the number of data. Which is one array.
@@ -28,15 +29,25 @@ for file in positive_reviews:
     print(splitfile[1].split(".")[0])
 
 
+def get_text(positive_reviews, score):
+    # Join together the text in the reviews for a particulat tone.
+    # Use lowercase to avoid "Not" and "not bering seens as different words.
+    return " ".join([r[0].lower() for r in positive_reviews if r[1] == str(score)])
 
 
-
-# Counting every file in the folder
-# Making sure we get all 12500 positive reviews in the training folder.
-# counter = 1
-# for file in positive_reviews:
-#   print(counter)
-#   counter += 1
+def count_text(text):
+    # Split text into words based on whitespace.
+    words = re.split("\s+", text)
+    # Count up the occurence of each word.
+    return Counter(words)
 
 
+negative_text = get_text(positive_reviews, -1)
+positive_text = get_text(positive_reviews, 1)
+# Generate word counts for negative tone.
+negative_counts = count_text(negative_text)
+# Generate word counts for positive tone.
+positive_counts = count_text(positive_text)
 
+print("Negative text sample: {0}".format(negative_text[:100]))
+print("Positive text sample: {0}".format(positive_text[:100]))
