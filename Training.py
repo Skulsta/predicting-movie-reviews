b4c5589c66ea2aeac7fa91bae8abe46ec719cd11
Skulsta <sti.sku@gmail.com>
2018-03-13 13:01:06 +0100

    Getting text from every file in pos training

--- Training.py
+++ Training.py
@@ -22,6 +22,21 @@ print(os.path.isfile("aclImdb/train/pos/0_9.txt"))
 positive_reviews = os.listdir("aclImdb/train/pos")
 print(positive_reviews)
 
+
+def get_real_text(file_path):
+    # Some files are not encoded correctly, ignoring those who fail to satisfy us.
+    file_text = open(file_path, 'r', encoding='UTF-8', errors='ignore')
+
+    text_lines = file_text.readlines()
+    print(text_lines)
+    file_text.close()
+
+
+def get_all_files():
+    file_path = "aclImdb/train/pos/" + file
+    get_real_text(file_path)
+
+
 # Split every filename by underscore, then split the second half and retrieve the review score.
 counter = 1
 for file in positive_reviews:
@@ -30,8 +45,8 @@ for file in positive_reviews:
 
 
 def get_text(positive_reviews, score):
-    # Join together the text in the reviews for a particulat tone.
-    # Use lowercase to avoid "Not" and "not bering seens as different words.
+    # Join together the text in the reviews for a particular tone.
+    # Use lowercase to avoid "Not" and "not" being seen as different words.
     return " ".join([r[0].lower() for r in positive_reviews if r[1] == str(score)])
 
 
@@ -48,6 +63,10 @@ positive_text = get_text(positive_reviews, 1)
 negative_counts = count_text(negative_text)
 # Generate word counts for positive tone.
 positive_counts = count_text(positive_text)
+
+# Test stuff
+print(negative_text)
+print(positive_text)
 
 print("Negative text sample: {0}".format(negative_text[:100]))
 print("Positive text sample: {0}".format(positive_text[:100]))
