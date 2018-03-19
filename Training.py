e08f6f25fb5d371085de2fceded70ea1973d015a
Skulsta <sti.sku@gmail.com>
2018-03-19 11:58:47 +0100

    Optimalisering med eksponensial

--- Training.py
+++ Training.py
@@ -145,17 +145,11 @@ def get_word_weight(text):
         print(count_text(every_positive_word).get(word))
         print(count_text(every_negative_word).get(word))
         if not count_text(every_positive_word).get(word) is None and word_occurences_in_positive_review > 100:
-            index = 0
-            while index < text_counts.get(word):
-                positive_word_weight = (word_occurences_in_positive_review) / number_of_positive_words
-                print("Was checked in positive")
-                index += 1
+            positive_word_weight = ((word_occurences_in_positive_review) / number_of_positive_words) ^ 2
+            print("Was checked in positive")
         if not count_text(every_negative_word).get(word) is None and word_occurences_in_negative_review > 100:
-            index = 0
-            while index < text_counts.get(word):
-                negative_word_weight = (word_occurences_in_negative_review) / number_of_negative_words
-                print("Was checked in negative")
-                index += 1
+            negative_word_weight = ((word_occurences_in_negative_review) / number_of_negative_words) ^ 2
+            print("Was checked in negative")
     product_of_positive *= positive_word_weight
     product_of_negative *= negative_word_weight
     print("Product of weight of positive and negative")
